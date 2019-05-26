33987050a4efac75526ac1bd1527de8f0d346419
Maciej Fiałkowski <fialek85@gmail.com>
2019-05-26 10:47:01 +0200

    added TopScroll, section Transformation

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 const navList = [...document.querySelectorAll(".nav__list li a")];
+const topButton = document.querySelector(".top-button");
 
 const clearClass = () => {
   navList.forEach(item => item.classList.remove("active"));
@@ -9,4 +10,26 @@ navList.forEach(item => {
     clearClass();
     e.target.classList.toggle("active");
   });
+});
+let h = window.innerHeight;
+
+const position = window.addEventListener("scroll", () => {
+  h = window.innerHeight;
+  window.pageYOffset > h
+    ? (topButton.style.display = "block")
+    : (topButton.style.display = "none");
+});
+
+const scrollToTop = () => {
+  const c = document.documentElement.scrollTop;
+  if (c > 0) {
+    let value = 16;
+    c > 30 ? (value = 16) : (value = 32);
+    window.requestAnimationFrame(scrollToTop);
+    window.scrollTo(0, c - c / value);
+  }
+};
+
+topButton.addEventListener("click", () => {
+  scrollToTop();
 });
